c832f0545da24ccebcd732921ccf5b1a72c2a0da
AezardSR <69091066+AezardSR@users.noreply.github.com>
2021-06-16 11:03:50 +0200

    hover en javascript

--- asset/js/btnRecheche.js
+++ asset/js/btnRecheche.js
@@ -4,6 +4,9 @@ btnRecheche = document.getElementById('RechercheButton');
 function changeButton() {
     btnRecheche.style.background = '#001A34';
 }
+function resetColor(){
+    btnRecheche.style.background = '#003468';
+}
 
-btnRecheche.onclick = function() {changeButton()}
-btnRecheche.style.background = '#003468';
+btnRecheche.onmouseover = function() {changeButton()}
+btnRecheche.onmouseout = function() {resetColor()}
